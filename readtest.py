255943a060dcb35927b6e759876a625971db33e5
ujtk6014 <ujtk6014.m2@keio.jp>
2021-10-01 12:33:37 +0900

    add readtest

--- readtest.py
+++ readtest.py
@@ -8,4 +8,4 @@ cat.APRD(IDX=0x00, ADP=ADP, ADO=ADDR, DATA=[0,0,0,0,0,0,0,0]) #DATAは０を８�
 (DATA, WKC) = cat.socket_read() #結果を読出し
 print("[0x{:04X}]= 0x{:02x}{:02x},0x{:02x}{:02x},0x{:02x}{:02x},0x{:02x}{:02x}".format(ADDR, DATA[7],DATA[6],DATA[5],DATA[4],DATA[3],DATA[2],DATA[1],DATA[0]))
 #読み出したデータを表示する
-print('/n' + str(DATA))
+print('\n' + str(DATA))
