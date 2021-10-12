f4b0646479abac43af793b34379d7e700a8f7874
ujtk6014 <ujtk6014.m2@keio.jp>
2021-10-12 18:18:11 +0900

    ADD: add some captured data

--- cap_and_ana/packet_cap.sh
+++ cap_and_ana/packet_cap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 FILENAME=sample
 DATE=`date "+%F-%T"`
-PCAP_FILE=../cap_result/${FILENAME}_${DATE}.pcap
+PCAP_FILE=./cap_result/${FILENAME}_${DATE}.pcap
 PCAP_IFACE=eth0
 PROTO=0x88a4
 
