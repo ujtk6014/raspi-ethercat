46b469fb73dee6fd0cb269300b5e76ca32ef803d
ujtk6014 <ujtk6014.m2@keio.jp>
2021-10-12 15:14:36 +0900

    UPDATE: create analysis

---
+++ cap_and_ana/packet_cap.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+FILENAME=sample
+DATE=`date "+%F-%T"`
+PCAP_FILE=./${FILENAME}_${DATE}.pcap
+PCAP_IFACE=eth0
+PROTO=0x88a4
+
+sudo /usr/sbin/tcpdump -w $PCAP_FILE -i $PCAP_IFACE ether proto $PROTO
